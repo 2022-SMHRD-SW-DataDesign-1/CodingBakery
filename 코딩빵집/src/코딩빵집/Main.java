75d6479880aa3ce954ea7b7f68090639ec4ea2ef
smhrd <smhrd@DESKTOP-KOD0C8B>
2022-09-05 15:44:33 +0900

    20220905 하영2

--- 코딩빵집/src/코딩빵집/Main.java
+++ 코딩빵집/src/코딩빵집/Main.java
@@ -5,10 +5,8 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Git 테스트입니다.");
 		System.out.println("김주희");
-		오류오류오류
-		이것도 올라갈까여~~~
-		ㄹㅇㅎㄴㅀㄴㅇㅀ
-
+		
+ System.out.println("코딩빵집 짱!");
 	}
 
 }
