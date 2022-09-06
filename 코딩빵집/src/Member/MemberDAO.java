7abcd917d4b6810c19203c4c46f986477d033979
smhrd <smhrd@DESKTOP-E3CL0P0>
2022-09-06 11:05:38 +0900

    11

--- 코딩빵집/src/Member/MemberDAO.java
+++ 코딩빵집/src/Member/MemberDAO.java
@@ -1,5 +1,61 @@
 package Member;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+
 public class MemberDAO {
+
+	Connection conn = null;
+	PreparedStatement psmt = null;
+	ResultSet rs = null;
+	boolean result;
+	
+	private void connect() {
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			
+			String url = "jdbc:oracle:thin:@localhost:1521:xe";
+			String id = "campus_g_0830_3";
+			String pw = "smhrd3";
+			
+			conn = DriverManager.getConnection(url, id, pw);
+			
+		} catch (ClassNotFoundException e) {
+			System.out.println("동적 로딩 실패");
+		} catch (SQLException e) {
+			System.out.println("DB 연결 실패");
+		}
+	}
+	
+	public int insert(MemberDTO dto) {
+		int cnt = 0;
+
+		connect();
+
+		try {
+			String name = dto.getName();
+			int num = dto.getNum();
+
+			String sql = "insert into member values(?, ?)";
+			psmt = conn.prepareStatement(sql);
+
+			psmt.setString(1, name);
+			psmt.setInt(2, num);
+
+			cnt = psmt.executeUpdate();
+
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return cnt;
+
+	}
+
 
 }
--- 코딩빵집/src/Member/MemberDTO.java
+++ 코딩빵집/src/Member/MemberDTO.java
@@ -2,4 +2,38 @@ package Member;
 
 public class MemberDTO {
 
+	// 필드
+	private String name;
+	private int num;
+
+	// get, set 메소드
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getNum() {
+		return num;
+	}
+
+	public void setNum(int num) {
+		this.num = num;
+	}
+
+	// 회원등록 메소드
+	public MemberDTO(String name, int num) {
+		super();
+		this.name = name;
+		this.num = num;
+	}
+
+	// 로그인 메소드
+	public MemberDTO(int num) {
+		super();
+		this.num = num;
+	}
+
 }
