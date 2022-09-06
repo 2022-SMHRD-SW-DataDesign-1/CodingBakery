b2f0652dfbb6403c958f9da6869691ca0386a553
smhrd <smhrd@DESKTOP-E3CL0P0>
2022-09-06 11:37:40 +0900

    memdto 1

--- 코딩빵집/src/Member/MemberDTO.java
+++ 코딩빵집/src/Member/MemberDTO.java
@@ -1,7 +1,35 @@
 package Member;
 
 public class MemberDTO {
-	
-	
-	
+
+	private String name;
+	int phone;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getPhone() {
+		return phone;
+	}
+
+	public void setPhone(int phone) {
+		this.phone = phone;
+	}
+
+	public MemberDTO(String name, int phone) {
+		super();
+		this.name = name;
+		this.phone = phone;
+	}
+
+	public MemberDTO(int phone) {
+		super();
+		this.phone = phone;
+	}
+
 }
